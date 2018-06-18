89b220531a599264425b87509bde0721ec621959
Philip Mattsson <maini.ac@hotmail.com>
2018-06-18 22:22:44 +0200

    Version 1

---
+++ Time_Counter/Time_Counter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time_Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Add in time in minutes: ");
+            string input = Console.ReadLine();
+
+            int Hour = int.Parse(input) / 60;
+            int Minute = int.Parse(input) - (Hour * 60);
+
+            string HourText = Hour == 1 ? "Hour" : "Hours";
+            string MinuteText = Minute == 1 ? "Minute" : "Minutes";
+
+            Console.WriteLine("Its " + Hour + " " + HourText + " " + "And " + Minute + " " + MinuteText);
+
+            Console.WriteLine("Enter any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
