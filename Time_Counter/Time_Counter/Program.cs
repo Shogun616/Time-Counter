04f66cc49b7a22c02914700dad3b1a4c6d77062f
Philip Mattsson <maini.ac@hotmail.com>
2018-06-19 21:37:51 +0200

    Version 2

--- Time_Counter/Time_Counter/Program.cs
+++ Time_Counter/Time_Counter/Program.cs
@@ -10,16 +10,23 @@ namespace Time_Counter
     {
         static void Main(string[] args)
         {
-            Console.Write("Add in time in minutes: ");
+            Console.Write("Insert the amount of time in minutes: ");
             string input = Console.ReadLine();
 
-            int Hour = int.Parse(input) / 60;
-            int Minute = int.Parse(input) - (Hour * 60);
+            int Year = int.Parse(input) / 518400;
+            int Month = int.Parse(input) / 43200;
+            int Day = int.Parse(input) / 1440;
+            int Hour = (int.Parse(input) - (Day * 1440))/ 60;
+            int Minute = (int.Parse(input) - ((Day * 24 * 60) + (Hour * 60)));
 
+            string YearText = Year == 1 ? "Year" : "Years";
+            string MonthText = Month == 1 ? "Month" : "Months";
+            string DayText = Day == 1 ? "Day" : "Days"; 
             string HourText = Hour == 1 ? "Hour" : "Hours";
             string MinuteText = Minute == 1 ? "Minute" : "Minutes";
 
-            Console.WriteLine("Its " + Hour + " " + HourText + " " + "And " + Minute + " " + MinuteText);
+            Console.WriteLine("The total time is " + " " + Year + " " + YearText+ " " + " " + Month + " " + MonthText + " " + Day + " " + 
+                DayText + " " + Hour + " " + HourText + " " + "And " + Minute + " " + MinuteText);
 
             Console.WriteLine("Enter any key to exit...");
             Console.ReadLine();
